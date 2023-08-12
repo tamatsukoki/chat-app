dec51ec4119d3a645d87ee91e698d57d8a1b7ea9
tamatsukoki <140251096+tamatsukoki@users.noreply.github.com>
2023-08-12 11:10:36 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'messages/index'
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
